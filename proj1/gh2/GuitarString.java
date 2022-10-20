c57091fd1aa1c5124cedb0d6d468222e422e0bdf
twopunchman <ray.wang.pro@gmail.com>
2022-10-20 13:04:26 -0400

    Updated GuitarString tic method

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,6 +1,7 @@
 package gh2;
 
 // uncomment the following import once you're ready to start this portion
+import deque.ArrayDeque;
 import deque.LinkedListDeque;
 import deque.Deque;
 // maybe more imports
@@ -24,7 +25,7 @@ public class GuitarString {
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer array with zeros.
         int dequeSize = (int) Math.round(this.SR / frequency);
-        this.buffer = new LinkedListDeque<Double>();
+        this.buffer = new ArrayDeque<>();
         
         for (int i = 0; i < dequeSize; i++) {
             this.buffer.addLast(0.0);
@@ -34,14 +35,14 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // Dequeue everything in buffer, and replace with random numbers
-        // between -0.5 and 0.5. You can get such a number by using:
-        // double r = Math.random() - 0.5;
+        //      Dequeue everything in buffer, and replace with random numbers
+        //      between -0.5 and 0.5. You can get such a number by using:
+        //      double r = Math.random() - 0.5;
         //
-        //       Make sure that your random numbers are different from each
-        //       other. This does not mean that you need to check that the numbers
-        //       are different from each other. It means you should repeatedly call
-        //       Math.random() - 0.5 to generate new random numbers for each array index.
+        //      Make sure that your random numbers are different from each
+        //      other. This does not mean that you need to check that the numbers
+        //      are different from each other. It means you should repeatedly call
+        //      Math.random() - 0.5 to generate new random numbers for each array index.
         double r = Math.random() - 0.5;
         for (int i = 0; i < this.buffer.size(); i++) {
             this.buffer.removeFirst();
@@ -56,19 +57,21 @@ public class GuitarString {
         // Dequeue the front sample and enqueue a new sample that is
         // the average of the two multiplied by the DECAY factor.
         // **Do not call StdAudio.play().**
-        
-        for (int j = 0; j < this.buffer.size(); j++) {
-            double first = this.buffer.get(0);
-            double second = this.buffer.get(1);
+
+        int j = 0;
+        while (j < this.buffer.size() - 1) {
+            double first = this.buffer.removeFirst();
+            double second = this.buffer.get(0);
             double newDouble = this.DECAY * 0.5 * (first + second);
-            this.buffer.removeFirst();
             this.buffer.addLast(newDouble);
+            j++;
         }
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
         // Return the correct thing.
-        return this.buffer.get(0);
+        int lastIndex = this.buffer.size() - 1;
+        return this.buffer.get(lastIndex);
     }
 }
