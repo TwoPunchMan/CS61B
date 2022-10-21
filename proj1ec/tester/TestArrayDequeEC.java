a3be1a9eafac09f2d00d330fb9423fd7850c5ce1
twopunchman <ray.wang.pro@gmail.com>
2022-10-21 17:29:19 -0400

    Added TestArrayDequeEC.java

---
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -0,0 +1,74 @@
+package tester;
+
+import static org.junit.Assert.*;
+import edu.princeton.cs.introcs.StdRandom;
+import org.junit.Test;
+import student.StudentArrayDeque;
+
+public class TestArrayDequeEC {
+
+    @Test
+    public void randomNumTest() {
+        StudentArrayDeque<Integer> S = new StudentArrayDeque<Integer>();
+        ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<Integer>();
+
+        int methodCallAmt = 100000;
+        String[] methodCalls = new String[methodCallAmt];
+
+        int i = 0;
+        while (i < methodCallAmt) {
+            String methodName = null;
+            Integer x = null;
+            Integer y = null;
+            int randNumberMethod = StdRandom.uniform(4);
+            int randNumberDeque = StdRandom.uniform(9) + 1;
+            if (randNumberMethod == 0) {
+                S.addFirst(randNumberDeque);
+                ad.addFirst(randNumberDeque);
+                methodName = String.format("addFirst(%d)",randNumberDeque);
+            } else if (randNumberMethod == 1) {
+                S.addLast(randNumberDeque);
+                ad.addLast(randNumberDeque);
+                methodName = String.format("addLast(%d)",randNumberDeque);
+            } else if (randNumberMethod == 2 || randNumberMethod == 3) {
+                if (ad.size() == 0) {
+                    i--;
+                    continue;
+                }
+
+                if (randNumberMethod == 2) {
+                    x = S.removeFirst();
+                    y = ad.removeFirst();
+                    methodName = "removeFirst()";
+                } else if (randNumberMethod == 3) {
+                    x = S.removeLast();
+                    y = ad.removeLast();
+                    methodName = "removeLast()";
+                }
+            }
+
+            if (methodName != null) {
+                methodCalls[i] = methodName;
+            }
+
+            if ((x != null && y != null) && x != y) {
+                String msg = printMsg(methodCalls);
+                assertEquals(msg ,x, y);
+            }
+
+            i++;
+        }
+    }
+
+    private String printMsg(String[] methodCalls) {
+        StringBuilder msg = new StringBuilder();
+        for (int i = 0; i < methodCalls.length; i++) {
+            if (methodCalls[i] != null) {
+                msg.append(methodCalls[i]);
+                msg.append("\n");
+            }
+        }
+
+        return msg.toString();
+    }
+}
