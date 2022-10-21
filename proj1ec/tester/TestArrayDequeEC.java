c0f4a5753b66c818056f17152e380df6845a4c80
twopunchman <ray.wang.pro@gmail.com>
2022-10-21 18:00:43 -0400

    Done with project 1 extra credit

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -6,15 +6,21 @@ import org.junit.Test;
 import student.StudentArrayDeque;
 
 public class TestArrayDequeEC {
+    private StudentArrayDeque<Integer> S = new StudentArrayDeque<Integer>();
+    private ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<Integer>();
+    private final int methodCallAmt = 1000000;
+    private String[] methodCalls = new String[methodCallAmt];
+    private boolean flag = false;
 
     @Test
     public void randomNumTest() {
-        StudentArrayDeque<Integer> S = new StudentArrayDeque<Integer>();
-        ArrayDequeSolution<Integer> ad = new ArrayDequeSolution<Integer>();
+        while (flag == false) {
+            runTest();
+            methodCalls = new String[methodCallAmt];
+        }
+    }
 
-        int methodCallAmt = 100000;
-        String[] methodCalls = new String[methodCallAmt];
-
+    private void runTest() {
         int i = 0;
         while (i < methodCallAmt) {
             String methodName = null;
@@ -32,7 +38,6 @@ public class TestArrayDequeEC {
                 methodName = String.format("addLast(%d)",randNumberDeque);
             } else if (randNumberMethod == 2 || randNumberMethod == 3) {
                 if (ad.size() == 0) {
-                    i--;
                     continue;
                 }
 
@@ -52,6 +57,7 @@ public class TestArrayDequeEC {
             }
 
             if ((x != null && y != null) && x != y) {
+                flag = true;
                 String msg = printMsg(methodCalls);
                 assertEquals(msg ,x, y);
             }
